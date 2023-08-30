53180923830071af7293415d2d3ac84d50313006
Renato Lousan da Silva <renatolousan@hotmail.com>
2023-08-30 00:01:29 -0400

    substituindo input e output

--- pagerank.py
+++ pagerank.py
@@ -1,4 +1,5 @@
 from pyspark import SparkConf, SparkContext
+import sys
 
 def compute_contribs(pair):
     _, (links, rank) = pair
@@ -10,7 +11,7 @@ def main():
     conf = SparkConf().setAppName("PageRank")
     sc = SparkContext(conf=conf)
 
-    links = sc.textFile("gs://$BUCKET_NAME/input_data").map(lambda line: tuple(line.split())).groupByKey().cache()
+    links = sc.textFile(sys.argv[1]).map(lambda line: tuple(line.split())).groupByKey().cache()
     ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
 
     NUM_ITERATIONS = 10
@@ -18,7 +19,7 @@ def main():
         contribs = links.join(ranks).flatMap(compute_contribs)
         ranks = contribs.reduceByKey(lambda x, y: x + y).mapValues(lambda rank: 0.15 + 0.85 * rank)
 
-    ranks.saveAsTextFile("gs://$BUCKET_NAME/output_data")
+    ranks.saveAsTextFile(sys.argv[2])
 
 if __name__ == "__main__":
     main()
